89ba0ee1cb4e0e9303df74ca0a9e227ab9961e13
eirikhr <erynning@gmail.com>
2018-11-05 16:30:09 +0100

    First commit pycharm

---
+++ RealPython/Part 1/src/functions.py
@@ -0,0 +1,19 @@
+# Testing functions
+
+def addisjon(no1, no2):
+    resultat = no1 + no2
+    return resultat
+
+
+def rot(no1):
+    resultat = no1 * no1
+    return resultat
+
+
+def gange(no1, no2):
+    resultat = no1 * no2
+    return resultat
+
+print(addisjon(5, 2))
+print(rot(3))
+print(gange(3, 2))
