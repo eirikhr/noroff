13ac87ca42b575c6835324863d3b3045771a44bd
eirikhr <erynning@gmail.com>
2018-11-05 18:47:41 +0100

    Funksjon for beregning av vekstfaktor på investering over x år.

---
+++ RealPython/Part 1/src/invest.py
@@ -0,0 +1,8 @@
+def invest(amount, rate, time):
+    print("Beløp som skal investeres:", str(amount))
+    print("Vekstfaktoren er:", str(rate))
+    for t in range(1, time + 1):
+        amount = amount * (1 + rate)
+        print("År", str(t), ":", str(amount))
+
+invest(10, .25, 3)
