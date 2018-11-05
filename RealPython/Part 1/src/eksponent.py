e88ba8c0c08b0fbfab3cde1e7230c3c4cfe6d656
eirikhr <erynning@gmail.com>
2018-11-05 18:54:08 +0100

    Beregner eksponenten av x

---
+++ RealPython/Part 1/src/eksponent.py
@@ -0,0 +1,7 @@
+# Mottar to input nummer og kalkulerer "opphøyd i"
+
+no1 = input("Nummer?")
+no2 = input("Opphøyd i?")
+resultat = float(no1) ** float(no2)
+
+print("{} opphøyd i {} = {}".format(no1, no2, resultat))
