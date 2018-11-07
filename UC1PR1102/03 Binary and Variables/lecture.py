47e54472b562298d767822652793b17b35e14f61
Eirik <erynning@gmail.com>
2018-11-07 11:22:15 +0100

    :)

---
+++ UC1PR1102/03 Binary and Variables/lecture.py
@@ -0,0 +1,10 @@
+'''
+
+var = 8
+print(var, type(var))
+var = 3.14
+print(var, type(var))
+var = "Norge"
+print(var, type(var))
+
+'''
--- UC1PR1102/Extra/snake.py
+++ UC1PR1102/Extra/snake.py
@@ -6,7 +6,7 @@ curses.curs_set(0)
 sh, sw = s.getmaxyx()
 w = curses.newwin(sh, sw, 0, 0)
 w.keypad(1)
-w.timeout(100)
+w.timeout(1)
 
 snk_x = sw/4
 snk_y = sh/2
